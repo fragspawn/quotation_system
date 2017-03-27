5c523464e0cdf083ba96865bb132024cbb70026d
developer <developer@webserver.tafeqld.dd>
2017-03-27 12:35:12 +1000

    javascript updates

--- quotepage.php
+++ quotepage.php
@@ -1,5 +1,43 @@
 <html>
     <head>
+    <style>
+        .img_me {display: none;}
+        .img_lg {display: none;}
+        .img_hu {display: none;}
+    </style>
+    <script>
+        function slideMe(slider) {
+            hide_all_images(slider);
+            document.getElementById('slider_output_' + slider.id).innerHTML = slider.value;
+
+            if(slider.value <= (document.getElementById('slider_unit_sm_' + slider.id).value)) {
+                document.getElementById('img_sm_' + slider.id).style.display = 'block';
+            }
+            if(slider.value > document.getElementById('slider_unit_sm_' + slider.id).value &&
+               slider.value < document.getElementById('slider_unit_me_' + slider.id).value) {
+                document.getElementById('img_me_' + slider.id).style.display = 'block';
+            }
+            if(slider.value >= document.getElementById('slider_unit_me_' + slider.id).value &&
+               slider.value < document.getElementById('slider_unit_lg_' + slider.id).value) {
+                document.getElementById('img_lg_' + slider.id).style.display = 'block';
+            }
+            if(slider.value >= document.getElementById('slider_unit_lg_' + slider.id).value &&
+               slider.value < document.getElementById('slider_unit_hu_' + slider.id).value) {
+                document.getElementById('img_lg_' + slider.id).style.display = 'block';
+            }            
+            if(slider.value >= document.getElementById('slider_unit_hu_' + slider.id).value) {
+                document.getElementById('img_hu_' + slider.id).style.display = 'block';
+            }
+        }
+
+        function hide_all_images(slider) {
+            document.getElementById('img_sm_' + slider.id).style.display = 'none';
+            document.getElementById('img_me_' + slider.id).style.display = 'none';
+            document.getElementById('img_lg_' + slider.id).style.display = 'none';
+            document.getElementById('img_hu_' + slider.id).style.display = 'none';
+        }
+
+    </script>
     </head>
     <body>
       <form action="submit.php" method="POST">
@@ -19,10 +57,9 @@
         <label><?php echo $row['system_name']; ?></label>
         <input type="range" name="<?php echo $row['system_name']; ?>" id="<?php echo $row['system_name']; ?>" value="<?php echo $row['units_min']; ?>"
           min="<?php echo $row['units_min']; ?>" max="<?php echo $row['units_max']; ?>" step="<?php echo $row['unit_increment']; ?>"
-          onChange="slider_output_<?php echo $row['system_name']; ?>.innerHTML = <?php echo $row['system_name']; ?>.value">
-        <output class="slider_output" id="slider_output_<?php echo $row['system_name']; ?>"></output><span><?php echo $row['units']; ?></span>
+          onChange="slideMe(this)">
+        <output class="slider_output" id="slider_output_<?php echo $row['system_name']; ?>"><?php echo $row['units_min']; ?></output><span><?php echo $row['units']; ?></span>
         <input type="hidden" name="slider_unit_cost_<?php echo $row['system_name']; ?>" id="slider_unit_cost_<?php echo $row['system_name']; ?>" value="<?php echo $row['unit_cost']; ?>">
-
         <input type="hidden" name="slider_unit_sm_<?php echo $row['system_name']; ?>" id="slider_unit_sm_<?php echo $row['system_name']; ?>" value="<?php echo $row['units_small']; ?>">
         <input type="hidden" name="slider_unit_me_<?php echo $row['system_name']; ?>" id="slider_unit_me_<?php echo $row['system_name']; ?>" value="<?php echo $row['units_medium']; ?>">
         <input type="hidden" name="slider_unit_lg_<?php echo $row['system_name']; ?>" id="slider_unit_lg_<?php echo $row['system_name']; ?>" value="<?php echo $row['units_large']; ?>">
