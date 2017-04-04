f59ca77e63971d7ac7c3b3c772d61d8fa01f0527
developer <developer@webserver.tafeqld.dd>
2017-04-04 12:56:35 +1000

    fix subtotal in JS

--- quotepage.php
+++ quotepage.php
@@ -6,6 +6,10 @@
         .img_hu {display: none;}
     </style>
     <script>
+        window.onload = function() {
+            calculate_total();
+        }
+
         function slideMe(slider) {
             hide_all_images(slider);
             document.getElementById('slider_output_' + slider.id).innerHTML = slider.value;
@@ -28,6 +32,18 @@
             if(Number(slider.value) >= Number(document.getElementById('slider_unit_hu_' + slider.id).value)) {
                 document.getElementById('img_hu_' + slider.id).style.display = 'block';
             }
+            calculate_total();
+        }
+
+        function calculate_total() {
+            var totalVal = 0; 
+            var subTot = 0;
+            var class_of_sliders = document.getElementsByClassName('item_range_element');
+            for(i=0;i<class_of_sliders.length;i++) {
+                subTot = class_of_sliders[i].value * document.getElementById('slider_unit_cost_' + class_of_sliders[i].id).value; 
+                totalVal += subTot;
+            } 
+            document.getElementById('quote_total').innerHTML = 'TOTAL = $' + totalVal.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
         }
 
         function hide_all_images(slider) {
@@ -42,7 +58,7 @@
     <body>
       <form action="submit.php" method="POST">
 <?php
-    $db = new PDO("mysql:host=localhost;dbname=quotesys;charset=utf8","root","NeeT0467");
+    $db = new PDO("mysql:host=localhost;dbname=quotesys;charset=utf8","root","");
     $sql = "SELECT * FROM line_item";
     $sth = $db->prepare($sql);
     $sth->execute();
@@ -55,7 +71,7 @@
         <img class="img_lg" id="img_lg_<?php echo $row['system_name']; ?>" src="<?php echo $row['image_large']; ?>"/>
         <img class="img_hu" id="img_hu_<?php echo $row['system_name']; ?>" src="<?php echo $row['image_massive']; ?>"/>
         <label><?php echo $row['system_name']; ?></label>
-        <input type="range" name="<?php echo $row['system_name']; ?>" id="<?php echo $row['system_name']; ?>" value="<?php echo $row['units_min']; ?>"
+        <input type="range" class="item_range_element" name="<?php echo $row['system_name']; ?>" id="<?php echo $row['system_name']; ?>" value="<?php echo $row['units_min']; ?>"
           min="<?php echo $row['units_min']; ?>" max="<?php echo $row['units_max']; ?>" step="<?php echo $row['unit_increment']; ?>"
           onChange="slideMe(this)">
         <output class="slider_output" id="slider_output_<?php echo $row['system_name']; ?>"><?php echo $row['units_min']; ?></output><span><?php echo $row['units']; ?></span>
@@ -71,5 +87,6 @@
 ?>
   <input type="submit" value="submit">
 </form>
+<div id="quote_total"></div>
 </body>
 </html>
