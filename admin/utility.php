68f171bac5b961630d3b40a4bbf3d718828ec067
developer <developer@webserver.tafeqld.dd>
2017-09-05 13:21:23 +1000

    security validation

--- admin/utility.php
+++ admin/utility.php
@@ -1,13 +1,69 @@
 <?php
+/*
+ * What method should be best suited to protect our server from unwanted user input
+ * Write a function that cleans up, and checks input against an ideal kind of input
+ *
+ * This is a research exercise, as much as anything, to ensure you are dealing with
+ * input that is assumed by design, but users may have less than ideal intent for
+ * your server.
+ *
+ * <?php session_destroy(); ?>
+ * <script>Some code here</script>
+ * 
+ * $x[] = 'foo';
+ * 
+ */
+
 
 function util_validity_check($untrusted, $regex) {
-    $trusted = $untrusted;
+    $trusted = stripslashes($untrusted);
+
+    $trusted = preg_replace('/<\?*(.*?)\?>/is', "", $trusted);
+    $trusted = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', "", $trusted);
+    $trusted = preg_replace('<!--*-->', '', $trusted); 
+    $trusted = preg_replace('/\/\/(.*?)/is', "", $trusted);
+
+    $trusted = strip_tags($trusted);
+    $trusted = htmlspecialchars($trusted);
+    $trusted = trim($trusted); 
+
+// do a regex
+    
     return $trusted;
+
 }
 
+
+/*
+ * A reliable, secret password encryption technique is mandatory for Internet facing sites
+ *
+ * Problem is, that most encryption techniques are insecure
+ *
+ * PHP is one of the few languages that get this right.
+ * Can you find the technique for encrypting passwords
+ * SECURELY?
+ *  MD5
+ *  SHA256
+ *  
+ */
+
 function encrypt_password($p) {
-    $new_p = $p;
+    // Option 2
+    $new_p = hash($p, 'The quick brown fox jumped over the lazy dog.');
     return $new_p;
 }
 
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
